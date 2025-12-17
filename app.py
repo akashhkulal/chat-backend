aaed6498e7d38440b4a4f106f6449c8bf1f1136e
akashhkulal <akashkulal88@gmail.com>
2025-12-17 15:47:12 +0530

    finaaaal

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import os
 app = Flask(__name__)
 CORS(app)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///chat.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///chat_v2.db'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
